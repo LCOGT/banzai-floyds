3a8e723121ed40a8a3e653617f5119af8acd66b5
Curtis McCully <cmccully@lco.global>
2025-01-09 14:44:03 -0500

    Added a way to dynamically choose the x-range for orders by storing the locations in the database instead of hard coding.

--- banzai_floyds/dbs.py
+++ banzai_floyds/dbs.py
@@ -1,5 +1,5 @@
-from banzai.dbs import Base
-from sqlalchemy import Column, Integer, String, Float, create_engine
+from banzai.dbs import Base, add_or_update_record
+from sqlalchemy import Column, Integer, String, Float, create_engine, ForeignKey, DateTime, desc
 from banzai.dbs import get_session, add_or_update_record
 from astropy.coordinates import SkyCoord
 from astropy import units
@@ -9,6 +9,8 @@ import pkg_resources
 from glob import glob
 import os
 from astropy.io import fits
+import datetime
+from banzai.utils.date_utils import parse_date_obs
 
 
 def get_standard(ra, dec, runtime_context, offset_threshold=5):
@@ -55,6 +57,17 @@ class FluxStandard(Base):
     dec = Column(Float)
 
 
+class OrderLocation(Base):
+    __tablesname__ = 'orderlocations'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    instrument_id = Column(Integer, ForeignKey("instruments.id"), index=True)
+    good_until = Column(DateTime, default=datetime.datetime(3000, 1, 1))
+    good_after = Column(DateTime, default=datetime.datetime(1000, 1, 1))
+    order_id = Column(Integer)
+    xmin = Column(Integer)
+    xmax = Column(Integer)
+
+
 def create_db(db_address):
     # Create an engine for the database
     engine = create_engine(db_address)
@@ -73,3 +86,87 @@ def ingest_standards(db_address):
                           'ra': standard_hdu[0].header['RA'],
                           'dec': standard_hdu[0].header['DEC']}
             add_or_update_record(db_session, FluxStandard, attributes, attributes)
+
+
+def get_order_locations(dateobs, order_id, instrument, db_address):
+    with get_session(db_address) as db_session:
+        location_query = db_session.query(OrderLocation).filter(OrderLocation.instrument_id == instrument.id)
+        location_query = location_query.filter(OrderLocation.order_id == order_id)
+        location_query = location_query.filter(OrderLocation.good_after <= dateobs)
+        location_query = location_query.filter(OrderLocation.good_until >= dateobs)
+        location_query.order_by(desc(OrderLocation.id))
+        order_location = location_query.first()
+        order_location = [order_location.xmin, order_location.xmax]
+    return order_location
+
+
+def add_order_location(db_address, instrument_id, xmin, xmax, order_id, good_after, good_until):
+    """ Add the x range (location) to use for a given order/instrument.
+
+    We cover 4 cases:
+    - Replace the current running location (good until = inf):
+        - The new location will become the current running location
+        - The old location will retain its good after date and set its good until to the good after of the new location
+    - The new range dates fall within a an exisiting range (good until != inf):
+        - We split the exisiting location, one with good after being the original good after and good until being the
+          start of the new location, and the other with good after being the end of the new location and good until
+          the original value.
+    - The new location starts and ends before an overlapping location:
+        - The exisiting location record has good after set to good until from the new location
+    - The new location starts and ends after an overlapping location:
+        - The existing location's good until is set to the good after of the new location
+    """
+
+    good_until = parse_date_obs(good_until)
+    good_after = parse_date_obs(good_after)
+    with get_session(db_address) as db_session:
+        # Case 1: Replace the current running location
+        if good_until > datetime.datetime(2100, 1, 1):
+            running_location = db_session.query(OrderLocation).filter(OrderLocation.instrument_id == instrument_id)
+            running_location = running_location.filter(OrderLocation.order_id == order_id)
+            running_location = running_location.filter(OrderLocation.good_until >= datetime.datetime(2100, 1, 1))
+            running_location = running_location.first()
+            if running_location is not None:
+                running_location.good_until = good_after
+                db_session.add(running_location)
+            db_session.commit()
+        else:
+            # Case 2: New record falls entirely inside an existing one
+            overlapping_locations = db_session.query(OrderLocation).filter(OrderLocation.instrument_id == instrument_id)
+            overlapping_locations = overlapping_locations.filter(OrderLocation.order_id == order_id)
+            overlapping_locations = overlapping_locations.filter(OrderLocation.good_after <= good_after)
+            overlapping_locations = overlapping_locations.filter(OrderLocation.good_until >= good_until)
+            overlapping_locations = overlapping_locations.all()
+            for location in overlapping_locations:
+                split_location = OrderLocation(instrument_id=instrument_id, xmin=location.xmin, xmax=location.xmax,
+                                               order_id=order_id, good_after=good_until, good_until=location.good_until)
+                db_session.add(split_location)
+                location.good_until = good_after
+                db_session.add(location)
+                db_session.commit()
+            # Case 3: New record starts and ends before an overlapping location
+            overlapping_locations = db_session.query(OrderLocation).filter(OrderLocation.instrument_id == instrument_id)
+            overlapping_locations = overlapping_locations.filter(OrderLocation.order_id == order_id)
+            overlapping_locations = overlapping_locations.filter(OrderLocation.good_after >= good_after)
+            overlapping_locations = overlapping_locations.filter(OrderLocation.good_until >= good_until)
+            overlapping_locations = overlapping_locations.all()
+            for location in overlapping_locations:
+                location.good_after = good_until
+                db_session.add(location)
+                db_session.commit()
+            # Case 4: New record starts and ends after an overlapping location
+            overlapping_locations = db_session.query(OrderLocation).filter(OrderLocation.instrument_id == instrument_id)
+            overlapping_locations = overlapping_locations.filter(OrderLocation.order_id == order_id)
+            overlapping_locations = overlapping_locations.filter(OrderLocation.good_after <= good_after)
+            overlapping_locations = overlapping_locations.filter(OrderLocation.good_until <= good_until)
+            overlapping_locations = overlapping_locations.all()
+            for location in overlapping_locations:
+                location.good_until = good_after
+                db_session.add(location)
+                db_session.commit()
+
+        # After all that create the new location record
+        new_location = OrderLocation(instrument_id=instrument_id, xmin=xmin, xmax=xmax, order_id=order_id,
+                                     good_after=good_after, good_until=good_until)
+        db_session.add(new_location)
+        db_session.commit()
--- banzai_floyds/orders.py
+++ banzai_floyds/orders.py
@@ -7,7 +7,7 @@ from banzai.data import ArrayData, DataTable
 from astropy.table import Table
 from astropy.io import fits
 from scipy.special import expit
-
+from banzai_floyds.dbs import get_order_locations
 from banzai_floyds.matched_filter import optimize_match_filter
 from copy import deepcopy
 
@@ -405,9 +405,6 @@ class OrderSolver(Stage):
     CENTER_CUT_WIDTH = 31
     POLYNOMIAL_ORDER = 3
 
-    ORDER_REGIONS = {'ogg': [(0, 1550), (500, 1835)],
-                     'coj': [(55, 1600), (615, 1920)]}
-
     def do_stage(self, image):
         if image.orders is None:
             # Try a blind solve if orders doesn't exist
@@ -424,14 +421,16 @@ class OrderSolver(Stage):
                                                  self.ORDER_HEIGHT,
                                                  order_center,
                                                  image.data.shape[1] // 2)
+                order_region = get_order_locations(image.dateobs, i + 1, image.instrument,
+                                                   self.runtime_context.db_address)
                 order_region = self.ORDER_REGIONS[image.site]
-                good_region = np.logical_and(x >= order_region[i][0],
-                                             x <= order_region[i][1])
+                good_region = np.logical_and(x >= order_region[0],
+                                             x <= order_region[1])
                 initial_model = Legendre.fit(deg=self.POLYNOMIAL_ORDER,
                                              x=x[good_region],
                                              y=order_locations[good_region],
-                                             domain=(order_region[i][0],
-                                                     order_region[i][1]))
+                                             domain=(order_region[0],
+                                                     order_region[1]))
                 order_estimates.append((initial_model.coef, self.ORDER_HEIGHT, initial_model.domain))
         else:
             # Load from previous solve
